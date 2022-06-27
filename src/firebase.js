2c263899e91f5f740a16468cab03d94d4c40608f
Mrinal kanti sardar <mrinalkantisardar007@gmail.com>
2022-06-27 23:49:35 +0530

    firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -8,3 +8,12 @@ const firebaseConfig = {
     appId: "1:851341005971:web:b565883fa67aa23959a966",
     measurementId: "G-7QMR4NRH99"
   };
+  
+  // Initialize Firebaseapp
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firevbase.auth();
+  const provider = new firebase.auth.GooleAuthProvider();
+
+  export { auth, provider };
+  export default db;
